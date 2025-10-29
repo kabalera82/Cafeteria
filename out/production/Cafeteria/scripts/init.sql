028b1c40a83a2465f869bc32155e0da9b004cccf
kabalera82 <kabalera@gmail.com>
2025-10-29 14:16:03 +0100

    🧹 Limpieza: eliminar .idea, out, target y aplicar .gitignore

--- out/production/Cafeteria/scripts/init.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS cafeteria;
-USE CAFETERIA;
-
-CREATE TABLE Cliente(
-                idCliente CHAR(36) PRIMARY KEY DEFAULT (UUID()),
-                nombre VARCHAR(100),
-                direccion VARCHAR(100),
-                telefono VARCHAR(15)
-                    );
